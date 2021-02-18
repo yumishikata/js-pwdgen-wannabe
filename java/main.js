b00bb51f239ae1756137097e6267c58f8d22fb0a
Yumi Shikata <yumishika17@gmail.com>
2021-02-18 14:05:07 +0100

    Push

---
+++ java/main.js
@@ -0,0 +1,15 @@
+const numero = 19;
+
+var nome;
+var cognome;
+var colore;
+var password;
+
+nome = prompt('Inserisci il tuo nome: ');
+
+cognome = prompt('Inserisci il tuo cognome: ');
+
+colore = prompt('Inserisci il tuo colore preferito: ');
+
+document.getElementById('my_id').innerHTML =
+password = nome + cognome + colore + numero; 
